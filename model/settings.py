8b21819b717d48a7ca05eb3558aa1f262a6ce2b2
Andrei <z@sp.sp>
2021-09-24 03:50:35 +0300

    first commit

---
+++ model/settings.py
@@ -0,0 +1,11 @@
+#export CUDA_VISIBLE_DEVICES=3
+import os
+
+data_dir = '/data/5_patexia/image_classifier/dataset/'
+batch_size = 4
+num_workers = 4
+topk = 2
+
+SHOW_BAR = False
+DEBUG = True
+TOPk = 3
