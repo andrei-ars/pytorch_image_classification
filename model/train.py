6581c2adcd90a7b15074b3492dd8bfab3f042bd7
Andrei <z@sp.sp>
2021-09-24 09:30:23 +0300

    2

--- model/train.py
+++ model/train.py
@@ -137,25 +137,26 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     return model
 
 
+if __name__ == "__main__":
 
-model_ft = models.resnet18(pretrained=True)
-num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, num_classes)
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model_ft = model_ft.to(device)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model_ft = model_ft.to(device)
 
-criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
 
-# Observe that all parameters are being optimized
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-    num_epochs=10)
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+        num_epochs=10)
 
-# save model
-model_path = 'model.pt'
-torch.save(model_ft.state_dict(), model_path)
+    # save model
+    model_path = 'model.pt'
+    torch.save(model_ft.state_dict(), model_path)
